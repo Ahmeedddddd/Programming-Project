13b2fff9ea428fad72d41451ed60e6ca3449ffa9
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-06-05 21:09:37 +0200

    added routes and working on javascript

---
+++ src/JS/main.js
@@ -0,0 +1,3 @@
+export function hideLoading(){
+    const hideLoading = () => loadingOverlay && (loadingOverlay.style.display = 'none');
+}
--- src/Server/app.js
+++ src/Server/app.js
@@ -6,9 +6,18 @@ const app = express();
 // Serveer statische frontendbestanden
 app.use(express.static(path.join(__dirname, '../CareerLaunch')));
 
+<<<<<<< Updated upstream
 // Voorbeeld API endpoint
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hallo van de backend!' });
+=======
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use('/src/CSS', express.static(path.join(__dirname, '../CSS')));
+app.use('/src/JS', express.static(path.join(__dirname, '../JS')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+>>>>>>> Stashed changes
 });
 
 app.listen(3000, () => {
