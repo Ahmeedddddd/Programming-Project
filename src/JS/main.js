c88825f8555c0b3f840007d33c595438c5e2b265
tombomeke-ehb <tom.dekoning@student.ehb.be>
2025-06-06 09:36:57 +0200

    Fixed merging problems

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,8 +1,4 @@
 export function hideLoading(){
-<<<<<<< Updated upstream
-    const hideLoading = () => loadingOverlay && (loadingOverlay.style.display = 'none');
-=======
     const loadingOverlay = document.getElementById('loadingOverlay');
     loadingOverlay.style.display = 'none';
->>>>>>> Stashed changes
 }
