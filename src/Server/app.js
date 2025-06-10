9b212a6ed04c1836a72183acec2fe114111547bd
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2025-06-10 10:57:01 +0200

    Update app.js

--- src/Server/app.js
+++ src/Server/app.js
@@ -7,9 +7,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../CareerLaunch')));
 
 app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../CSS')));
+app.use(express.static(path.join(__dirname, '../JS')));
+
 // Voorbeeld API endpoint
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hallo van de backend!' });
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public.html'));
 });
@@ -59,8 +60,6 @@ app.get('/programma', (req, res) => {
   res.sendFile(path.join(__dirname, '../../src/HTML/PROGRAMMA/programma.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server draait op http://localhost:3000');
 app.get('/programmaVoormidag', (req, res) => {
   res.sendFile(path.join(__dirname, '../../src/HTML/PROGRAMMA/programma-voormidag.html'));
 });
@@ -104,3 +103,14 @@ const { sendInvoice } = require('./mail'); // of pad naar juiste locatie
 app.use(express.json()); // Zorg dat je JSON-body’s kunt ontvangen
 
 app.post('/api/send-invoice', async (req, res) => {
+  try {
+    await sendInvoice(req.body);
+    res.status(200).json({ message: '✅ Factuur verzonden!' });
+  } catch (err) {
+    console.error('❌ Fout bij verzenden:', err);
+    res.status(500).json({ message: 'Verzenden mislukt' });
+  }
+});
+
+
+app.listen(port, () => console.log(`Server has started on: http://localhost:${port}`))
