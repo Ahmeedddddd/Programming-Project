e8bbe3840af9a922851d25d3fa1eabcafcc547a5
Ahmedtkd <ahmed.takadoumi@student.ehb.be>
2025-06-15 17:15:05 +0200

    Linken van de studenten Homepagina met de testfile van de localserver om de verandereingen te zien

--- src/Server/app.js
+++ src/Server/app.js
@@ -19,6 +19,11 @@ app.use('/src/JS', express.static(path.join(__dirname, '../JS')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+app.get('/student-homepage', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../src/HTML/STUDENTEN/student-homepage.html'));
+});
+
+
 
 app.get('/test', (req, res) => {
   res.sendFile(path.join(__dirname, '../../src/HTML/test.html'));
